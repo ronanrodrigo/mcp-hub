d006f74b108ca0a3e80bccb23c144a905ec17827
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2026-08-09 01:02:39 -0300

    Remove obsolete test helper

--- tests/helpers.js
+++
@@ -1,3 +0,0 @@
-export function mockResponse() {
-  return { statusCode: 200, body: null, status(code) { this.statusCode = code; return this; }, json(value) { this.body = value; return this; } };
-}
